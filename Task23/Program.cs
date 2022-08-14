3e3fb91b10c81278565cba957a39d8940bab9acc
anna <slukin1@yandex.ru>
2022-08-14 18:11:26 +0700

    tesk23

---
+++ Task23/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("  x    x**3 ");
+
+void PrintCube(int b)
+{
+    for (int i = 1; i <= b; i++)
+        Console.WriteLine($"{i, 3}  {i * i * i, 4}");
+}
+if (number < 1)
+{
+    Console.WriteLine("Введите положительное число");
+    return;
+}
+PrintCube(number);
+
+
+
+// Console.WriteLine("Введите число N: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number > 0)  
+// {
+//     Console.WriteLine("  x    x**3 ");
+//     for(int i=1; i<=number; i++)
+//     {
+//        Console.WriteLine($"{i, 3}  {i*i*i, 4}"); 
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Введите положительное число");
+// }
